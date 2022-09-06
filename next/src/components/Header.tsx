1de24ca416e4a6a4ae432267a18e5a7a8bd35917
koheitakahashi <426koohei@gmail.com>
2022-09-07 07:05:53 +0900

    一覧ページにデザインを充てた
    動的な機能は未実装。

---
+++ next/src/components/Header.tsx
@@ -0,0 +1,23 @@
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+const Header = () => {
+  return (
+    <div>
+      <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Typography
+          component="h1"
+          variant="h4"
+          color="inherit"
+          align="left"
+          noWrap
+          sx={{ flex: 1 }}
+        >
+          HIKIDO
+        </Typography>
+      </Toolbar>
+    </div>
+  );
+}
+
+export default Header

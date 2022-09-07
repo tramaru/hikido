8c15b71d92ec554574736bb09c024b807f1b9d3d
koheitakahashi <kouhei.takahashi@everyleaf.com>
2022-09-08 07:29:43 +0900

    不要なimportを削除

--- next/src/components/Events.tsx
+++ next/src/components/Events.tsx
@@ -1,5 +1,4 @@
 import Header from 'components/Header'
-import SearchForm from 'components/SearchForm' // TODO: 「全文検索機能」実装時に実装する」
 import Event from 'components/Event';
 import Grid from '@mui/material/Grid';
 import Divider from '@mui/material/Divider';

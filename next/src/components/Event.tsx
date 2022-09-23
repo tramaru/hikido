1c2850cd072553d99916a801e044323148bb86e4
koheitakahashi <426koohei@gmail.com>
2022-09-23 16:48:47 +0900

    transcript を fetch したら、即時にボタンの非表示とtranscript の表示ができるようにした
    それに伴って、component 間のデータの流れを見直した。変更前のコンポーネント構成では、
    TranscribeButton が transcript を持つことになり、親コンポーネントはそれを検知できなかった。
    親コンポーネントが transcript を表示させることになるため、TranscribeButton コンポーネントを廃止した。
    
    また、useState を使って、状態を管理するようにした。

--- next/src/components/Event.tsx
+++ next/src/components/Event.tsx
@@ -4,12 +4,16 @@ import Card from '@mui/material/Card';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import Box from '@mui/material/Box';
-import TranscribeButton from 'components/TranscribeButton';
 import { EventProps } from 'types/EventProps';
+import Button from '@mui/material/Button';
+import { Dispatch, SetStateAction, useState } from 'react';
 
 type Props = { event: EventProps }
 
 export default function Event(props: Props) {
+  const [transcript, setTranscript] = useState(props.event.transcript)
+  const [_buttonDisplay, setButtonDisplay] = useState(transcript === "")
+
   return (
     <Grid item xs={12} md={6}>
       <CardActionArea component='a' href='#'>
@@ -22,10 +26,17 @@ export default function Event(props: Props) {
               { props.event.title }
             </Typography>
             <Box mt={1.5}>
-              { renderTranscript(props.event.transcript) }
+              { renderTranscript(transcript) }
             </Box>
             <Box mt={1.5}>
-              { renderTranscribeButton(props.event.transcript, props.event.id) }
+              {
+                renderTranscribeButton(
+                  props.event.id,
+                  transcript,
+                  setTranscript,
+                  setButtonDisplay,
+                )
+              }
             </Box>
           </CardContent>
         </Card>
@@ -44,8 +55,40 @@ const renderTranscript = (transcript: string | undefined) => {
   }
 }
 
-const renderTranscribeButton = (transcript: string | undefined, eventId: number) => {
-  if (transcript === undefined || transcript === "") {
-    return <TranscribeButton eventId={eventId} />
+const renderTranscribeButton = (
+  eventId: number,
+  transcript: string | undefined,
+  setTranscript: Dispatch<SetStateAction<string | undefined>>,
+  setButtonDisplay: Dispatch<SetStateAction<boolean>>,
+) => {
+  if (transcript === "" || transcript === undefined) {
+    return (
+      <Button
+        variant='contained'
+        onClick={(e) => updateEventTranscript(e, eventId, setTranscript, setButtonDisplay)}
+      >
+        文字起こしをする
+      </Button>
+    );
   }
-}
+};
+
+const updateEventTranscript = async (
+  _event: React.MouseEvent<HTMLElement>,
+  eventId: number,
+  setTranscript: Dispatch<SetStateAction<string | undefined>>,
+  setButtonDisplay: Dispatch<SetStateAction<boolean>>,
+) => {
+  const response = await fetch(`/api/events/${eventId}/transcript`, {
+    method: 'PUT',
+  });
+
+  const { event, error } = await response.json();
+  if (response.ok) {
+    setTranscript(event.transcript)
+    setButtonDisplay(false)
+  } else {
+    // TODO: エラー時の処理を追加
+    console.log('Error', error);
+  }
+};

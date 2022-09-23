16a2e70a0c85c41f3944e40a608fef5cfb96d35f
koheitakahashi <426koohei@gmail.com>
2022-09-23 14:39:01 +0900

    文字起こしボタンを押したら、transcript が更新されるようにした

--- next/src/components/TranscribeButton.tsx
+++ next/src/components/TranscribeButton.tsx
@@ -2,6 +2,20 @@ import Button from '@mui/material/Button';
 
 export default function TranscribeButton() {
   return (
-    <Button variant='contained'>文字起こしする</Button>
+    <Button onClick={(e) => updateEventTranscript(e, props.eventId) } variant='contained'>文字起こしする</Button>
   )
 }
+
+const updateEventTranscript = async (event: React.MouseEvent<HTMLElement>, eventId: number) => {
+  const response = await fetch(`/api/events/${eventId}/transcript`, {
+    method: 'PUT',
+  })
+
+  const { data, errors } = await response.json()
+  if (!response.ok) {
+    // TODO: エラー時の処理を追加
+    console.log('Error', errors)
+  }
+}
+
+export default TranscribeButton

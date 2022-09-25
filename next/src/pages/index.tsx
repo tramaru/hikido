bea733728f9524b77a633edb6ec745fb25990c25
koheitakahashi <426koohei@gmail.com>
2022-09-25 12:08:01 +0900

    α版リリースように DB アクセスしないようにした
    α版をリリースしたいが、RDS に接続する前提のため、DATABASE_URL に設定されている DB に接続できずにアプリが落ちている。
    そのため、α版は DB に接続しないようにして、静的データを表示するようにした。

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -4,16 +4,45 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Header from 'components/Header'
-import { PrismaClient } from '@prisma/client';
-import { GetServerSideProps } from "next";
-import type { Event } from 'types/Event';
+// import { PrismaClient } from '@prisma/client';
+// import { GetServerSideProps } from "next";
+// import type { Event } from 'types/Event';
 import type { NextPage } from 'next'
 
 const theme = createTheme();
 
-type Props = { events: Event[] }
+const Home: NextPage = () => {
+  const events = [
+    {
+      id: 1,
+      title: '第1回 OSSコードリーディング会(テスト)',
+      audioUrl: "",
+      transcriptUrl: "",
+      transcript: "",
+      createdAt: "2021-09-01T00:00:00.000Z",
+      updatedAt: "2021-09-01T00:00:00.000Z",
 
-const Home: NextPage<Props> = (props: Props) => {
+    },
+    {
+      id: 2,
+      title: '第2回 OSSコードリーディング会(テスト)',
+      audioUrl: "s3://amazon-transcribe-sample/1662270844167.ogg",
+      transcriptUrl: "",
+      transcript: "",
+      createdAt: "2021-09-01T00:00:00.000Z",
+      updatedAt: "2021-09-01T00:00:00.000Z",
+    },
+    {
+      id: 3,
+      title: '第3回 OSSコードリーディング会(テスト)',
+      audioUrl: "sample.ogg",
+      transcriptUrl: "sample.json",
+      transcript: "Pundit を読むぞ。Machine learning is employed in a range of computing tasks where designing and programming explicit algorithms with good performance is difficult or infeasible. Example applications include email filtering, detection of network intruders and computer vision.",
+      createdAt: "2021-09-01T00:00:00.000Z",
+      updatedAt: "2021-09-01T00:00:00.000Z",
+    },
+  ];
+
   return (
     <div>
       <Head>
@@ -24,7 +53,7 @@ const Home: NextPage<Props> = (props: Props) => {
         <Container maxWidth='lg'>
           <Header />
           <main>
-            <EventList events={ props.events } />
+            <EventList events={ events } />
           </main>
         </Container>
       </ThemeProvider>
@@ -32,15 +61,15 @@ const Home: NextPage<Props> = (props: Props) => {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const prisma = new PrismaClient();
-  const selectedEvents = await prisma.event.findMany();
-  const events = JSON.parse(JSON.stringify(selectedEvents));
-  return {
-    props: {
-      events,
-    }
-  }
-}
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const prisma = new PrismaClient();
+//   const selectedEvents = await prisma.event.findMany();
+//   const events = JSON.parse(JSON.stringify(selectedEvents));
+//   return {
+//     props: {
+//       events,
+//     }
+//   }
+// }
 
 export default Home

9e2e865b509c6f395bbdb90b4863073fd14a0e48
koheitakahashi <426koohei@gmail.com>
2022-09-25 10:33:29 +0900

    不要なコードを削除

--- next/src/components/TranscribeButton.tsx
+++ next/src/components/TranscribeButton.tsx
@@ -15,22 +15,4 @@ const TranscribeButton = (props: Props) => {
   );
 }
 
-// const updateEventTranscript = async (
-//   _event: React.MouseEvent<HTMLElement>,
-//   eventId: number,
-//   setTranscript: Dispatch<SetStateAction<string | undefined>>,
-//   setErrorMessage: Dispatch<SetStateAction<boolean>>,
-// ) => {
-//   const response = await fetch(`/api/events/${eventId}/transcript`, {
-//     method: 'PUT',
-//   });
-
-//   const { event } = await response.json();
-//   if (response.ok) {
-//     setTranscript(event.transcript)
-//   } else {
-//     setErrorMessage(true)
-//   }
-// }
-
 export default TranscribeButton

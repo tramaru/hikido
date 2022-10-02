10b69c4455006bf8b2052ed6f7b96d1965c5b208
otegami <a.s.takuya1026@gmail.com>
2022-10-03 08:31:40 +0900

    Removed express server
    I thought this app needs it to make heroku server reboot when heroku
    gets down. Indeed, Heroku server is working for all day.

--- discord_bot/src/index.ts
+++ discord_bot/src/index.ts
@@ -1,17 +1,1 @@
 import './bot/bot'
-import express, { Request, Response } from 'express'
-
-const { EXPRESS_PORT } = process.env
-
-// Set up Express server
-const app = express()
-
-if (!EXPRESS_PORT) {
-  console.error("Error: EXPRESS_PORT が設定されていません！")
-  process.exit(1)
-}
-
-app.use('/', (req: Request, res: Response) => {
-  res.sendStatus(200);
-})
-app.listen(EXPRESS_PORT, () => console.log(`Server started on port ${EXPRESS_PORT}!`));

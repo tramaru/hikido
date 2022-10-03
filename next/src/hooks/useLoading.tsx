6d7aef53e3be555fb5ab99eec92309863b68e2c9
koheitakahashi <426koohei@gmail.com>
2022-10-03 20:43:18 +0900

    useLoader のリファクタリング
    ref: https://github.com/tramaru/hikido/pull/44#discussion_r985311867

--- next/src/hooks/useLoading.tsx
+++ next/src/hooks/useLoading.tsx
@@ -1,24 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import type { NextRouter } from 'next/router'
 
 const useLoading = (router: NextRouter) => {
   const [isLoading, setIsLoading] = useState(false)
+  const handleStart = useCallback((url: string) => url !== router.asPath && setIsLoading(true), [router.asPath])
+  const handleComplete = useCallback(() => setIsLoading(false), [])
 
- useEffect(() => {
-    const handleStart = (url: string) => url !== router.asPath && setIsLoading(true)
-    const handleComplete = () => setIsLoading(false)
-
+  useEffect(() => {
     router.events.on('routeChangeStart', handleStart)
     router.events.on('routeChangeComplete', handleComplete)
     router.events.on('routeChangeError', handleComplete)
-
-    return () => {
-      router.events.off('routeChangeStart', handleStart)
-      router.events.off('routeChangeComplete', handleComplete)
-      router.events.off('routeChangeError', handleComplete)
-    }
-  })
-
+  }, [handleComplete, handleStart, router.events])
   return isLoading
 }
 

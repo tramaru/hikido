ed66606cf04e8e62ffdbd5eddf73dac30f2a9814
koheitakahashi <426koohei@gmail.com>
2022-10-08 15:06:40 +0900

    イベントのカードの高さを transcript のありなしに関係なく統一させた
    transcript がある場合とない場合のカードで高さが異なり、違和感があったため。

--- next/src/components/EventCard.tsx
+++ next/src/components/EventCard.tsx
@@ -1,6 +1,5 @@
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import DetailLinkButton from 'components/DetailLinkButton'
 import ErrorMessage from './ErrorMessage';
@@ -30,28 +29,36 @@ const EventCard: React.FC<{ event: Event }> = ({ event }) => {
 
   return (
     <Grid item xs={12} md={6}>
-      <CardActionArea component='div'>
-        <Card sx={{ display: 'flex' }}>
-          <CardContent sx={{ flex: 1 }}>
-            <Typography variant='subtitle1' color='text.secondary'>
-              {formatDateTime(event.createdAt)}
-            </Typography>
-            <Typography component='h2' variant='h5'>
-              {event.title}
-            </Typography>
-            <Box mt={1.5}>
-              {isTranscript && <Transcript transcript={transcript} />}
+      <Card variant='outlined' sx={{ display: 'flex', height: '12.5rem' }}>
+        <CardContent sx={{ flex: 1 }}>
+          <Typography variant='subtitle1' color='text.secondary'>
+            {formatDateTime(event.createdAt)}
+          </Typography>
+          <Typography component='h2' variant='h5' mb={1.5}>
+            {event.title}
+          </Typography>
+          {isTranscript && (
+            <Box mb={1.5}>
+              <Transcript transcript={transcript} />
             </Box>
-            <Box mt={1.5}>
-              {!isTranscript && <TranscribeButton onClick={refetch} />}
+          )}
+          {!isTranscript && (
+            <Box mb={1.5}>
+              <TranscribeButton onClick={refetch} />
             </Box>
-            <Box mt={1.5}>
-              {isTranscript && <DetailLinkButton onClick={eventDetailLink} />}
+          )}
+          {isTranscript && (
+            <Box mb={1.5}>
+              <DetailLinkButton onClick={eventDetailLink()} />
             </Box>
-            <Box mt={1.5}>{error && <ErrorMessage />}</Box>
-          </CardContent>
-        </Card>
-      </CardActionArea>
+          )}
+          {error && (
+            <Box mb={1.5}>
+              <ErrorMessage />
+            </Box>
+          )}
+        </CardContent>
+      </Card>
     </Grid>
   );
 };

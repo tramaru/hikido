fcc32f444be171990197c5a63831875fb26376e7
koheitakahashi <426koohei@gmail.com>
2022-10-09 13:31:01 +0900

    イベント詳細ページで getStaticProps・getStaticPaths を使わないようにした
    イベント詳細ページは build 時に固定なページではなく、ユーザー操作で増えるページである。
    そのため、予め静的にページを生成することはできない。よって、上記の変更をした。

--- next/src/pages/events/[id].tsx
+++ next/src/pages/events/[id].tsx
@@ -1,59 +1,41 @@
+import Box from '@mui/material/Box';
+import ErrorMessage from 'components/ErrorMessage';
 import EventDetail from 'components/EventDetail';
 import Grid from '@mui/material/Grid';
-import { PrismaClient } from '@prisma/client';
 
-import type {
-  NextPage,
-  GetStaticPaths,
-  GetStaticPropsContext,
-  InferGetStaticPropsType
-} from 'next';
+import type { NextPage } from 'next';
+import type { Event } from 'types/Event';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
-type EventPageProps = InferGetStaticPropsType<typeof getStaticProps>
+const EventPage: NextPage = () => {
+  const [event, setEvent] = useState<Event | undefined>()
+  const [error, setError] = useState<boolean>()
+  const router = useRouter()
+  const { id: eventId } = router.query
 
-const EventPage: NextPage<EventPageProps> = (
-  {
-    event,
-  }: EventPageProps
-) => {
+  useEffect(() => {
+    if (!eventId) return
+    const fetchEvent = async () => {
+      const response = await fetch(`/api/events/${eventId}`)
+      const { event: fetchedEvent, error: fetchedError } = await response.json()
+      setError(!!fetchedError)
+      setEvent(fetchedEvent)
+    }
+
+    fetchEvent()
+  }, [eventId])
+
   return (
     <Grid container spacing={2} sx={{ mt: 1 }}>
+      {error && (
+        <Box mb={1.5}>
+          <ErrorMessage />
+        </Box>
+      )}
       {event && <EventDetail event={event} />}
     </Grid>
   );
-}
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const prisma = new PrismaClient();
-  const results = await prisma.event.findMany({
-    select: {
-      id: true
-    }
-  });
-  await prisma.$disconnect();
-  const paths = results.map((result: { id: number }) => `/events/${result.id}`)
-
-  return { paths, fallback: false }
-}
-
-export const getStaticProps = async ({ params }: GetStaticPropsContext) => {
-  if (!params) { throw new Error('params is undefined') }
-
-  const eventId = Number(params.id)
-  const prisma = new PrismaClient();
-  const foundEvent = await prisma.event.findUnique({
-    where: {
-      id: eventId
-    }
-  })
-  await prisma.$disconnect();
-  const event = JSON.parse(JSON.stringify(foundEvent));
-
-  return {
-    props: {
-      event: event,
-    }
-  }
 }
 
 export default EventPage

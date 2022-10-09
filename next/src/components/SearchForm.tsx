e708d978733d2b49d544d868ae8af7f5d601652f
koheitakahashi <426koohei@gmail.com>
2022-10-09 18:46:10 +0900

    検索フォームのデザインを整えた

--- next/src/components/SearchForm.tsx
+++ next/src/components/SearchForm.tsx
@@ -1,15 +1,12 @@
-import SearchIcon from '@mui/icons-material/Search';
 import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
 import { useState } from 'react';
 import { KeyboardEvent } from 'react';
+import Grid from '@mui/material/Grid';
 
 type Search = (query: string) => void
 
 const SearchForm: React.FC<{ onClick: Search }> = ({ onClick}) => {
   const [query, setQuery] = useState("")
-  const isDisabled = (query === "")
 
   const fireSearchWhenPressedEnter = (event: KeyboardEvent<HTMLDivElement>) => {
     if (event.key === 'Enter') {
@@ -18,29 +15,23 @@ const SearchForm: React.FC<{ onClick: Search }> = ({ onClick}) => {
   };
 
   return (
-    <Grid>
-      <TextField
-        label='Filled'
-        color='info'
-        variant='filled'
-        InputProps={{
-          type: 'search',
-        }}
-        sx={{ backgroundColor: 'white' }}
-        onChange={(e) => setQuery(e.target.value)}
-        value={query}
-        onKeyPress={(e) => {
-          fireSearchWhenPressedEnter(e);
-        }}
-      />
-      <Button
-        disabled={isDisabled}
-        onClick={() => {
-          onClick(query);
-        }}
-      >
-        <SearchIcon />
-      </Button>
+    <Grid container sx={{ width: "30%", mx: 'auto', mt: 2 }}>
+      <Grid item xs={12}>
+        <TextField
+          label="Search"
+          color="primary"
+          variant='outlined'
+          InputProps={{
+            type: 'search',
+          }}
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={(e) => {
+            fireSearchWhenPressedEnter(e);
+          }}
+          sx={{ width: "100%" }}
+        />
+      </Grid>
     </Grid>
   );
 }
